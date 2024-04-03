dd32e11c39d769f67765ae40e87183191e34ecf3
maihdi <maihdi.elfassi@gmail.com>
2024-04-03 17:38:15 +0200

    ajout collegue

--- app.py
+++ app.py
@@ -1,42 +1,47 @@
 import streamlit as st
 import pandas as pd
 import duckdb as db
+import io
 
-st.write("""
-#SQL SRS
-Space Repetition System SQL Practicce
-""")
+csv = '''
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+'''
 
-option = st.selectbox(
-    "What would you like to review",
-    ["Joins", "GroupBy", "Windows Functions"],
-    index=None,
-    placeholder="Select a Theme"
-)
+beverages= pd.read_csv(io.StringIO(csv))
 
-st.write('You selected: ', option)
+csv2 = '''
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+'''
 
-data = {"a": [1, 2, 3], "b": [4, 5, 6]}
-df = pd.DataFrame(data)
+food_items = pd.read_csv(io.StringIO(csv2))
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+answer = """
+select * from beverages
+cross join food_items
+"""
+
+solution = db.sql(answer).df()
+
+st.header("enter your code: ")
+query = st.text_area(label="votre code sql ici", key="user_input")
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 
-
-with tab1:
-    st.dataframe(df)
-    query = st.text_area(label="Entrez votre requete")
-    st.write(f" Voici la requete entré: {query}")
-    query_result = db.query(query).df()
-    st.write(query_result)
-
-# st.header("A Cat")
-# st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
-    st.header("A Dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected:")
+    st.dataframe(solution)
 
 with tab3:
-    st.header("A Owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.write(answer)
